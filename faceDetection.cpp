144c848ba97b7262388a840f10ea13221fcb9283
alexandresablayrolles <alexandre.sablayrolles@gmail.com>
2014-11-05 14:10:08 +0100

    Removed eyes detection

--- faceDetection.cpp
+++ faceDetection.cpp
@@ -13,9 +13,7 @@
 
  /** Global variables */
  String face_cascade_name = "haarcascade_frontalface_alt.xml";
- String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
  CascadeClassifier face_cascade;
- CascadeClassifier eyes_cascade;
  string window_name = "Capture - Face detection";
  RNG rng(12345);
 
@@ -27,7 +25,6 @@
 
    //-- 1. Load the cascades
    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-   if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
    //-- 2. Read the video stream
    capture = cvCaptureFromCAM( -1 );
@@ -68,18 +65,7 @@ void detectAndDisplay( Mat frame )
     ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
 
     Mat faceROI = frame_gray( faces[i] );
-    std::vector<Rect> eyes;
-
-    //-- In each face, detect eyes
-    eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-
-    for( size_t j = 0; j < eyes.size(); j++ )
-     {
-       Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-       int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-       circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-     }
   }
-  //-- Show what you got
+
   imshow( window_name, frame );
- }
+}
