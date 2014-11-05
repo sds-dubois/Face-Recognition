f33498b536efaff2dde4e0e0549939f50ec47f65
alexandresablayrolles <alexandre.sablayrolles@gmail.com>
2014-11-05 14:20:05 +0100

    Did some refactoring. Program is still detecting face from webcam input

--- faceDetection.cpp
+++ faceDetection.cpp
@@ -9,7 +9,7 @@
  using namespace cv;
 
  /** Function Headers */
- void detectAndDisplay( Mat frame );
+vector<Rect> detectAndDisplay( Mat frame );
 
  /** Global variables */
  String face_cascade_name = "haarcascade_frontalface_alt.xml";
@@ -18,39 +18,48 @@
  RNG rng(12345);
 
  /** @function main */
- int main( int argc, const char** argv )
- {
-   CvCapture* capture;
-   Mat frame;
+int main( int argc, const char** argv )
+{
+    CvCapture* capture;
+    Mat frame;
 
-   //-- 1. Load the cascades
-   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+    //-- 1. Load the cascades
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
-   //-- 2. Read the video stream
-   capture = cvCaptureFromCAM( -1 );
-   if( capture )
-   {
-     while( true )
-     {
-   frame = cvQueryFrame( capture );
+    //-- 2. Read the video stream
+    capture = cvCaptureFromCAM( -1 );
+    if( capture )
+    {
+        while( true ){
+            frame = cvQueryFrame( capture );
 
-   //-- 3. Apply the classifier to the frame
-       if( !frame.empty() )
-       { detectAndDisplay( frame ); }
-       else
-       { printf(" --(!) No captured frame -- Break!"); break; }
+            //-- 3. Apply the classifier to the frame
+            if( !frame.empty() ){
+                vector<Rect> faces = detectAndDisplay( frame );
+                for( size_t i = 0; i < faces.size(); i++ ){
+                    Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+                    ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+                }
+                imshow( window_name, frame );
+            }
+            else{
+                printf(" --(!) No captured frame -- Break!");
+                break;
+            }
 
-       int c = waitKey(10);
-       if( (char)c == 'c' ) { break; }
-      }
-   }
-   return 0;
- }
+            int c = waitKey(10);
+            if( (char)c == 'c' ) {
+                break;
+            }
+        }
+    }
+    return 0;
+}
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
+vector<Rect> detectAndDisplay( Mat frame )
 {
-  std::vector<Rect> faces;
+  vector<Rect> faces;
   Mat frame_gray;
 
   cvtColor( frame, frame_gray, CV_BGR2GRAY );
@@ -59,13 +68,5 @@ void detectAndDisplay( Mat frame )
   //-- Detect faces
   face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
-  for( size_t i = 0; i < faces.size(); i++ )
-  {
-    Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-    ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-
-    Mat faceROI = frame_gray( faces[i] );
-  }
-
-  imshow( window_name, frame );
+  return faces;
 }
