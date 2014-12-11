d791d84b09d4a806358255f7dd14cac39aaa92fc
alexandresablayrolles <alexandre.sablayrolles@gmail.com>
2014-12-11 23:12:22 +0100

    Still cannot hack google limitations...see You Later!

--- googleAPIcrawler.py
+++ googleAPIcrawler.py
@@ -4,34 +4,45 @@ import simplejson
 import os
 import shutil
 import Image
+import imghdr
 
-personalites = {'barack obama', 'francois hollande', 'manuel valls'}
 n=30
-basepath = 'data/labeled/'
+basepath = 'datapython/'
 
-if os.path.exists(basepath):
-	shutil.rmtree(basepath)
-os.mkdir(basepath)
+if not os.path.exists(basepath):
+	os.mkdir(basepath)
+
+print os.walk(basepath)
+personalites = [name for name in os.listdir(basepath) if os.path.isdir(os.path.join(basepath, name))]
 
 for personalite in personalites:
 	print personalite
-	path = basepath+personalite.replace(" ", "_")+"/"
-	os.mkdir(path)
+	path = basepath+"/"+personalite
 	i=1
 	j=1
-	for i in range(1, n):
+	for i in range(15, n):
 		try:
 			url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
-						'v=1.0&q='+personalite.replace(" ", "%20")+'&userip=INSERT-USER-IP&start='+str(4*i))
-			request = urllib2.Request(url, None, {'Referer': 'http://www.google.com'})
+						'v=1.0&q='+personalite.replace(" ", "%20")+'&userip=129.104.222.33&start='+str(4*i))
+			request = urllib2.Request(url, None, {'Referer': 'http://www.github.com'})
 			response = urllib2.urlopen(request)
 			results = simplejson.load(response)
 			values = results['responseData']['results']
 			for result in values:
 				s = result['url']
 				name = s[(s.rindex('/')+1):]
-				filename = path+str(j)+"."+(((name.split("."))[-1]).split("%"))[0]
+				fileextension = (((name.split("."))[-1]).split("%"))[0].replace("e", "").lower()
+				filename = path+str(j)+"."+fileextension
 				urllib.urlretrieve(s, filename)
+				print(str(i)+" "+str(j))
+				try:
+					actualextension = imghdr.what(filename).replace("e", "")
+					if fileextension != actualextension:
+						print filename
+						os.remove(filename)
+				except Exception as e:
+					print filename
+
 				try:
 					image = Image.open(filename)
 				except:
